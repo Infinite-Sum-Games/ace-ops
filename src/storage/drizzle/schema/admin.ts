b34845e8954dfdbe2e37f643f1c62f7899b33a85
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-07-01 21:28:00 +0530

    Add: Admin schema

---
+++ src/storage/drizzle/schema/admin.ts
@@ -0,0 +1,18 @@
+import { pgEnum } from "drizzle-orm/pg-core";
+import { uuid, varchar, timestamp } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+const AccountStatus = pgEnum("accountStatus", ["ACTIVE", "INACTIVE", "DELETED"]);
+
+export const AdminTable = pgTable("admins", {
+    adminId: uuid("adminId").primaryKey().defaultRandom(),
+    firstName: varchar("firstName", { length: 100 }).notNull(),
+    middleName: varchar("middleName", { length: 100 }),
+    lastName: varchar("lastName", { length: 100 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull(),
+    password: varchar("password", { length: 255 }).notNull(),
+    accountStatus: AccountStatus("accountStatus").default("ACTIVE").notNull(),
+    createdAt: timestamp("createdAt").notNull(),
+    updatedAt: timestamp("updatedAt").notNull(),
+    deletedAt: timestamp("deletedAt")
+});
