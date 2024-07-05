d8e59fcef512fddf7710ce2703b99453e0febd9d
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-07-05 18:18:04 +0530

    Simplify storage-schema exports

---
+++ src/storage/schema/index.ts
@@ -0,0 +1,13 @@
+// All exports are handled here (makes importing easy)
+
+export { default as AdminTable } from "./admin";
+export { default as AdminLoginTable } from "./adminLogin";
+export { default as AlumniTable } from "./alumni";
+export { default as BlogTable } from "./blog";
+export { default as CampaignTable } from "./campaign";
+export { default as EventTable } from "./event";
+export { default as MailTable } from "./mailingList";
+export { default as RegistrationTable } from "./registration";
+export { default as RoleTable } from "./role";
+export { default as SubscriberTable } from "./subscribers";
+export { default as UserTable } from "./user";
