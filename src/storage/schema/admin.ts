1649ac5ab2db713be4ba8ba9a72d1a53d315670c
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-07-05 18:15:39 +0530

    Schema (update): Admin - remove passwd

--- src/storage/schema/admin.ts
+++ src/storage/schema/admin.ts
@@ -1,17 +1,16 @@
-import { pgEnum } from "drizzle-orm/pg-core";
-import { uuid, varchar, timestamp } from "drizzle-orm/pg-core";
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgEnum, uuid, varchar, timestamp, pgTable } from "drizzle-orm/pg-core";
 
 const AccountStatus = pgEnum("accountStatus", ["ACTIVE", "INACTIVE", "DELETED"]);
 
-export const AdminTable = pgTable("admins", {
+const AdminTable = pgTable("admins", {
     adminId: uuid("adminId").primaryKey().defaultRandom(),
     firstName: varchar("firstName", { length: 100 }).notNull(),
     middleName: varchar("middleName", { length: 100 }),
     lastName: varchar("lastName", { length: 100 }).notNull(),
     email: varchar("email", { length: 255 }).notNull(),
-    password: varchar("password", { length: 255 }).notNull(),
     accountStatus: AccountStatus("accountStatus").default("ACTIVE").notNull(),
     createdAt: timestamp("createdAt").notNull(),
     updatedAt: timestamp("updatedAt").notNull(),
 });
+
+export default AdminTable;
