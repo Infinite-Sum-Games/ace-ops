16904bf074e3934b535cdab33b614086cbf81825
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-07-05 19:58:33 +0530

    Schema (new): Campaign

---
+++ src/storage/schema/campaign.ts
@@ -0,0 +1,12 @@
+import { pgTable, serial, uuid } from "drizzle-orm/pg-core";
+import MailTable from "./mailingList";
+import SubscriberTable from "./subscribers";
+
+const CampaignTable = pgTable("campaigns", {
+    id: serial("id").primaryKey(),
+    listId: uuid("listId").notNull().references(() => MailTable.listId),
+    subscriberId: uuid("subId").notNull().references(() => SubscriberTable.subscriberId),
+    createdAt: timestamp("createdAt", { mode: "string" }).defaultNow().notNull(),
+});
+
+export default CampaignTable;
