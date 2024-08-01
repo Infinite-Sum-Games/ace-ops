398a398dad0b6f7556a0e1efa3dce53c1d62e526
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-08-01 23:31:04 +0530

    Add: Blog, Newsletter routes added

--- src/general/routes.ts
+++ src/general/routes.ts
@@ -7,4 +7,10 @@ router.post("/login", handleLogin);
 router.post("/register", handleRegistration);
 router.post("/register/otp", handleOTPVerification);
 
+router.get("/blog", )
+router.get("/blog/:blogId", )
+
+router.get("/newsletter", )
+router.get("/blog/:newsletterId", )
+
 export default router;
