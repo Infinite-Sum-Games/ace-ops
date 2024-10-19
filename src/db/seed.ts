81ea36663f040e746eedaf8a48ea8e3eb706d141
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-10-20 00:12:27 +0530

    Seed Setup

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,10 +1,47 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+const environment = process.env.NODE_ENV;
+
+async function userSeed() {
+
+}
+
+async function adminSeed() {
+
+}
+
+async function blogSeed() {
+
+}
+
+async function suggestionSeed() {
+
+}
+
+async function eventSeed() {
+
+}
+
+async function sponsorSeed() {
+
+}
+
+async function registrationSeed() {
+
+}
 
 async function main() {
-  // TODO: Add user-seeds
   console.log('Seed data created successfully');
+  if (environment === "development") {
+    userSeed();
+    adminSeed();
+    blogSeed();
+    eventSeed();
+    sponsorSeed();
+    registrationSeed();
+    suggestionSeed();
+  }
 }
 
 main()
